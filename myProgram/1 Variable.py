d2b3568298ea7b8d1a065ba2c6d02a056da341c6
AnuragDarji <127482974+AnuragDarji@users.noreply.github.com>
2023-03-11 21:37:53 +0530

    Create 1 Variable.py

---
+++ myProgram/1 Variable.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+name="Aman" # string variable
+age=22 # integer variable
+
+name="Anurag"
+age=20.0 # floating variable
+
+if(age>18.0):  
+    is_itadult=True # Boolean variable
+else: 
+    is_itadult=False
+    
+print(name)
+print(age)
+print(is_itadult)
